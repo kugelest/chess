4d81cc7183de2789b033211950e38b34e302dda2
Zdravko <zd.matic96@gmail.com>
2021-04-09 09:36:37 +0200

    Adding Playground and output of Playground

--- src/main/scala/Chess.scala
+++ src/main/scala/Chess.scala
@@ -4,7 +4,7 @@ object Chess {
   def main(args: Array[String]): Unit = {
     println("Chess")
 
-    val pp = Playground(8,8).playgroundString()
+    val pp = Playground().playgroundString()
     println(pp)
   }
 }
--- src/main/scala/model/Playground.scala
+++ src/main/scala/model/Playground.scala
@@ -1,21 +1,20 @@
 package model
 
 case class Playground(width: Int=8, height: Int=8){
-  def widthandHeightisset: Boolean = width != 0 & height != 0
+  def widthAndHeightSsSet: Boolean = width != 0 & height != 0
 
   def playgroundString(): String= {
     var line = ""
-    var pg = ""
+    var pg = "  "
     var currentNum = ""
 
-    for (x <- 1 to height){
-      pg = pg.concat(("A"(0) + x - 1).toChar.toString + line)
+    for (x <- 1 to width){
+      pg = pg.concat(("A"(0) + x - 1).toChar.toString + "  ")
       line = line.concat(" . ")
     }
 
-    for (x <- 1 to width){
-      currentNum = currentNum.concat(x.toString + "  ")
-
+    for (x <- 1 to height){
+      currentNum = currentNum.concat(x.toString + line + x.toString + "\n")
     }
     "%s\n%s%s".format(pg, currentNum, pg)
   }
