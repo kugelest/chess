b587f06984e6fdbe1c12c7afe2929f03f6981ad0
kugelest <stefan.kugele@gmail.com>
2021-04-14 14:23:49 +0200

    main-method updated

--- src/main/scala/Chess.scala
+++ src/main/scala/Chess.scala
@@ -1,8 +1,12 @@
+import model._
 
 object Chess {
   def main(args: Array[String]): Unit = {
     println("Chess")
 
+    val board = new Board()
+    val board2 = board.startPosition()
+    println(board2)
 
   }
 }
