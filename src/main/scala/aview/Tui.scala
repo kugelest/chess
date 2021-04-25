64b31e4f6fbeff1ab9ff9d64c3a787bbef514447
kugelest <stefan.kugele@gmail.com>
2021-04-25 14:59:36 +0200

    view update

--- src/main/scala/aview/Tui.scala
+++ src/main/scala/aview/Tui.scala
@@ -8,6 +8,16 @@ class Tui {
     input match {
       case "s" => board.startPosition()
       case "q" => board
+      case input if input.contains("move") =>
+        val from2 = s"${input(5)}${input(6)}"
+        val to = s"${input(8)}${input(9)}"
+
+        board.move(from2, to)
     }
   }
+
+
+//  def checkInput(from: String, to: String): String = {
+//    val from = input.f
+//  }
 }
