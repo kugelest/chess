6b7fd93528b21b261bb7ee871ebc487bf533fbf5
kugelest <stefan.kugele@gmail.com>
2021-04-29 18:53:05 +0200

    square simplified

--- src/main/scala/model/Square.scala
+++ src/main/scala/model/Square.scala
@@ -1,7 +1,9 @@
 package model
 
 case class Square(pos: String, piece: Option[Piece]) {
-  def getPiece: Option[Piece] = piece
+  //def getPiece: Option[Piece] = piece
+  def getFile: Char = pos.head
+  def getRank: Int = pos.tail.toInt
   def isFree: Boolean = piece.isDefined
   def mannedByWhite(): Boolean = piece.get.color.equals('w')
   def mannedByBlack(): Boolean = piece.get.color.equals('b')
