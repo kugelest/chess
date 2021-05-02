f4180351f149f6c2c32eb6d1ffd724e3c1c5ca45
Zdravko <zd.matic96@gmail.com>
2021-05-02 14:56:39 +0200

    Updating Chess class in main branch

--- src/main/scala/Chess.scala
+++ src/main/scala/Chess.scala
@@ -1,5 +1,20 @@
+import model._
+import aview.Tui
+import controller.Controller
+
+import scala.io.StdIn.readLine
+
 object Chess {
+  val controller = new Controller(new Board(8))
+  val tui = new Tui(controller)
+  controller.notifyObservers()
+
   def main(args: Array[String]): Unit = {
-    println("Chess")
-  }
+      var input: String = ""
+
+      do {
+        input = readLine()
+        tui.processInputLine(input)
+      } while (input != "quit")
+    }
 }
