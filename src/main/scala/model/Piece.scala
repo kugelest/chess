221c85a30a74143a5135dcf4e01dd51a0f6ec907
kugelest <stefan.kugele@gmail.com>
2021-05-13 22:04:44 +0200

    factory

--- src/main/scala/model/Piece.scala
+++ src/main/scala/model/Piece.scala
@@ -8,3 +8,15 @@ trait Piece {
   def getFile: Char = pos.head
   def getRank: Int = pos.tail.toInt
 }
+
+object Piece {
+ def apply(kind: String, pos: String, color: Char): Piece = kind match {
+   case "king" => King(pos, color)
+   case "queen" => Queen(pos, color)
+   case "rook" => Queen(pos, color)
+   case "knight" => Queen(pos, color)
+   case "bishop" => Queen(pos, color)
+   case "pawn" => Queen(pos, color)
+ }
+}
+
