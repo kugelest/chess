74199d425393f365bcac9fbbfcbf598ece120190
kugelest <stefan.kugele@gmail.com>
2021-05-14 00:19:43 +0200

    movePossible

--- src/main/scala/model/Knight.scala
+++ src/main/scala/model/Knight.scala
@@ -2,42 +2,44 @@ package model
 
 case class Knight(pos: String, color: Char, kind: String = "knight") extends Piece {
 
-  override def whiteMovePossible(to: String, board: Board): Boolean = true
-  override def blackMovePossible(to: String, board: Board): Boolean = true
+  override def whiteMovePossible(to: String, board: Board): Boolean = {
+    val (destFile, destRank, origFile, origRank) = convertMove(to)
+    color match {
+      case 'w' =>
+        if(board.getSquare(to).isFree || board.getSquare(to).mannedByBlack()){ // Check if square is free,manned by black or white
+          if(destFile.equals((origFile + 1).toChar) || destFile.equals((origFile - 1).toChar)){ // Check if knight is moving 1 square left or right
+            if(destRank.equals(origRank + 2) || destRank.equals(origRank - 2)){ // Check if knight is moving 2 squares up or down
+              true
+            } else false // Change players turn
+          } else if (destFile.equals((origFile + 2).toChar) || destFile.equals((origFile - 2).toChar)){ // Check if knight is moving 2 squares left or right
+            if(destRank.equals(origRank + 1) || destRank.equals(origRank - 1)){ // Check if knight is moving 1 square up or down
+              true
+            } else false // Change players turn
+          } else false // Change players turn
+        } else false // Change players turn
+      case _ => false
+    }
+  }
 
-//  def move(to: String, board: Board): Board = {
-//    val destFile = to.charAt(0)
-//    val destRank = to.charAt(1).asDigit
-//    val origFile = pos.charAt(0)
-//    val origRank = pos.charAt(1).asDigit
-//
-//    color match {
-//      case 'w' =>
-//        if(board.getSquare(to).isFree || board.getSquare(to).mannedByBlack()){ // Check if square is free,manned by black or white
-//          if(destFile.equals((origFile + 1).toChar) || destFile.equals((origFile - 1).toChar)){ // Check if knight is moving 1 square left or right
-//            if(destRank.equals(origRank + 2) || destRank.equals(origRank - 2)){ // Check if knight is moving 2 squares up or down
-//              board.removePiece(pos).setPiece(Knight(to, color))
-//            } else board.setTurn(color) // Change players turn
-//          } else if (destFile.equals((origFile + 2).toChar) || destFile.equals((origFile - 2).toChar)){ // Check if knight is moving 2 squares left or right
-//            if(destRank.equals(origRank + 1) || destRank.equals(origRank - 1)){ // Check if knight is moving 1 square up or down
-//              board.removePiece(pos).setPiece(Knight(to, color))
-//            } else board.setTurn(color) // Change players turn
-//          } else board.setTurn(color) // Change players turn
-//        } else board.setTurn(color) // Change players turn
-//      case 'b' =>
-//        if(board.getSquare(to).isFree || board.getSquare(to).mannedByWhite()){ // Check if square is free,manned by black or white
-//          if(destFile.equals((origFile + 1).toChar) || destFile.equals((origFile - 1).toChar)){ // Check if knight is moving 1 square left or right
-//            if(destRank.equals(origRank + 2) || destRank.equals(origRank - 2)){ // Check if knight is moving 2 squares up or down
-//              board.removePiece(pos).setPiece(Knight(to, color))
-//            } else board.setTurn(color) // Change players turn
-//          } else if (destFile.equals((origFile + 2).toChar) || destFile.equals((origFile - 2).toChar)){ // Check if knight is moving 2 squares left or right
-//            if(destRank.equals(origRank + 1) || destRank.equals(origRank - 1)){ // Check if knight is moving 1 square up or down
-//              board.removePiece(pos).setPiece(Knight(to, color))
-//            } else board.setTurn(color) // Change players turn
-//          } else board.setTurn(color) // Change players turn
-//        } else board.setTurn(color) // Change players turn
-//    }
-//  }
+  override def blackMovePossible(to: String, board: Board): Boolean = {
+    val (destFile, destRank, origFile, origRank) = convertMove(to)
+    color match {
+      case 'b' =>
+        if(board.getSquare(to).isFree || board.getSquare(to).mannedByWhite()){ // Check if square is free,manned by black or white
+          if(destFile.equals((origFile + 1).toChar) || destFile.equals((origFile - 1).toChar)){ // Check if knight is moving 1 square left or right
+            if(destRank.equals(origRank + 2) || destRank.equals(origRank - 2)){ // Check if knight is moving 2 squares up or down
+              true
+            } else false // Change players turn
+          } else if (destFile.equals((origFile + 2).toChar) || destFile.equals((origFile - 2).toChar)){ // Check if knight is moving 2 squares left or right
+            if(destRank.equals(origRank + 1) || destRank.equals(origRank - 1)){ // Check if knight is moving 1 square up or down
+              true
+            } else false // Change players turn
+          } else false // Change players turn
+        } else false // Change players turn
+      case _ => false
+    }
+  }
+
 
   override def toString: String = {
     color match {
