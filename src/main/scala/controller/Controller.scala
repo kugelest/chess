a0f7d6e17549829e340e85aac4923dd2797850f6
kugelest <stefan.kugele@gmail.com>
2021-05-14 12:00:56 +0200

    state pattern implementation

--- src/main/scala/controller/Controller.scala
+++ src/main/scala/controller/Controller.scala
@@ -1,39 +1,44 @@
 package controller
 
-import controller.GameStatus.{BLACK_TURN, GameStatus, WHITE_TURN}
 import model.Board
 import util.Observable
 
-class Controller(var board: Board, var gameStatus: GameStatus = WHITE_TURN) extends Observable{
+class Controller(var board: Board, var gameStatus: GameStatus) extends Observable {
+
+  def this(board: Board) = this(board, new White_Turn(this))
+
+  def setGameStatus(newStatus: GameStatus): Unit = {
+    gameStatus = newStatus
+  }
 
   def createEmptyBoard(size: Int = 8):Unit = {
     board = new Board(size)
     notifyObservers()
   }
 
-  def createStartPosition(): Unit = {
-    createEmptyBoard()
-    board = board.startPosition()
-    gameStatus = WHITE_TURN
-    notifyObservers()
-  }
-
-  def move(from: String, to: String): Unit = {
-    gameStatus match {
-      case WHITE_TURN =>
-        if (board.whiteMovePossible(from, to)) {
-          board = board.move(from, to)
-          gameStatus = BLACK_TURN
-        }
-      case BLACK_TURN =>
-        if (board.blackMovePossible(from, to)) {
-          board = board.move(from, to)
-          gameStatus = WHITE_TURN
-        }
-      case _ =>
-    }
-    notifyObservers()
-  }
+//  def createStartPosition(): Unit = {
+//    createEmptyBoard()
+//    board = board.startPosition()
+//    gameStatus = WHITE_TURN
+//    notifyObservers()
+//  }
+//
+//  def move(from: String, to: String): Unit = {
+//    gameStatus match {
+//      case WHITE_TURN =>
+//        if (board.whiteMovePossible(from, to)) {
+//          board = board.move(from, to)
+//          gameStatus = BLACK_TURN
+//        }
+//      case BLACK_TURN =>
+//        if (board.blackMovePossible(from, to)) {
+//          board = board.move(from, to)
+//          gameStatus = WHITE_TURN
+//        }
+//      case _ =>
+//    }
+//    notifyObservers()
+//  }
 
   def boardToString(): String = {
     board.toString
--- src/main/scala/controller/GameStatus.scala
+++ src/main/scala/controller/GameStatus.scala
@@ -1,16 +1,40 @@
 package controller
 
-object GameStatus extends Enumeration {
-  type GameStatus = Value
-  val WIN_WHITE, WIN_BLACK, DRAW, WHITE_TURN, BLACK_TURN = Value
+import model.Board
 
-  val map = Map[GameStatus, String](
-    WIN_WHITE -> "White has won the Match!",
-    WIN_BLACK -> "Black has won the Match!",
-    DRAW -> "Game ended in a draw!"
-  )
+trait GameStatus {
+  def move(from: String, to: String): Board
+}
 
-  def message(gameStatus: GameStatus) = {
-    map(gameStatus)
+class White_Turn(controller: Controller) extends GameStatus {
+  override def move(from: String, to: String): Board = {
+    if (controller.board.whiteMovePossible(from, to)) {
+      controller.board = controller.board.move(from, to)
+      controller.setGameStatus(new Black_Turn(controller))
+    }
   }
 }
+
+class Black_Turn(controller: Controller) extends GameStatus {
+  override def move(from: String, to: String): Board = {
+    if (controller.board.blackMovePossible(from, to)) {
+      controller.board = controller.board.move(from, to)
+      controller.setGameStatus(new White_Turn(controller))
+    }
+  }
+}
+
+//object GameStatus extends Enumeration {
+//  type GameStatus = Value
+//  val WIN_WHITE, WIN_BLACK, DRAW, WHITE_TURN, BLACK_TURN = Value
+//
+//  val map = Map[GameStatus, String](
+//    WIN_WHITE -> "White has won the Match!",
+//    WIN_BLACK -> "Black has won the Match!",
+//    DRAW -> "Game ended in a draw!"
+//  )
+//
+//  def message(gameStatus: GameStatus) = {
+//    map(gameStatus)
+//  }
+//}
