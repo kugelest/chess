41d171502f1100f20c17e2cba636db2c1ff18dce
ZdravkoMat <zd.matic96@gmail.com>
2021-05-21 09:00:57 +0200

    cleaned up

--- src/main/scala/controller/BoardMemento.scala
+++ src/main/scala/controller/BoardMemento.scala
@@ -2,11 +2,9 @@ package controller
 
 import controller.states.State
 import model.Board
-import model.pieces.Piece
 import util.Memento
 
 class BoardMemento(state: State, board: Board) extends Memento {
-//  private val chessboard =  board
 
   override def getState: State = {
     state
--- src/main/scala/controller/MoveCommand.scala
+++ src/main/scala/controller/MoveCommand.scala
@@ -8,15 +8,11 @@ class MoveCommand(from: String, to: String, val controller: Controller) extends 
   var memento = new BoardMemento(controller.state, controller.board)
 
   override def doStep(): Unit = {
-    //memento = new BoardMemento()
     controller.state.move(from, to)
   }
   override def undoStep(): Unit = {
-//    controller.createEmptyBoard()
     controller.state = memento.getState
     controller.board = memento.getBoard
-//    controller.board.startPosition(memento.getController)
-    //controller.board = controller.board.move(from, to)
   }
   override def redoStep(): Unit = {
     controller.state.move(from, to)
