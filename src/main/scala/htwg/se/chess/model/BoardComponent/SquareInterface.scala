3c5e52418316e65deb1d2aedbda05f7118533bd4
kugelest <stefan.kugele@gmail.com>
2021-06-25 10:16:56 +0200

    fileio merge

--- src/main/scala/htwg/se/chess/model/BoardComponent/SquareInterface.scala
+++ src/main/scala/htwg/se/chess/model/BoardComponent/SquareInterface.scala
@@ -1,5 +1,9 @@
 package htwg.se.chess.model.BoardComponent
 
+import htwg.se.chess.model.BoardComponent.BoardBaseImpl.Piece
+
 trait SquareInterface {
+  def pos: String
+  def piece: Piece
   def isFree: Boolean
 }
